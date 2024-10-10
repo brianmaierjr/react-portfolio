3bdd9e82a8048f69a0abba7af9c3c65cad272a7b
Brian Maier <brimaidesigns@gmail.com>
2024-10-10 10:01:32 -0400

    + toggle and localstorage

--- src/components/color-mode-toggle/index.tsx
+++ src/components/color-mode-toggle/index.tsx
@@ -1,18 +1,44 @@
-import { useEffect } from "react";
-import useThemeStore from "../../store/themeStore";
+import "./color-mode-toggle.css";
 
-const ColorModeToggle = () => {
-	const { isDarkMode, toggleTheme } = useThemeStore();
+const DarkMode = () => {
+	const setDarkMode = () => {
+		document.querySelector("body")?.setAttribute("data-theme", "dark");
+		localStorage.setItem("selectedTheme", "dark");
+	};
 
-	useEffect(() => {
-		document.body.classList.toggle("dark", isDarkMode);
-	}, [isDarkMode]);
+	const setLightMode = () => {
+		document.querySelector("body")?.setAttribute("data-theme", "light");
+		localStorage.setItem("selectedTheme", "light");
+	};
+
+	const selectedTheme = localStorage.getItem("selectedTheme");
+
+	if (
+		selectedTheme === "dark" ||
+		window.matchMedia("(prefers-color-scheme: dark)")
+	) {
+		setDarkMode();
+	}
+
+	const toggleTheme = (e) => {
+		if (e.target.checked) setDarkMode();
+		else setLightMode();
+	};
 
 	return (
-		<div className='toggle-container'>
-			<button onClick={toggleTheme}>Toggle Theme</button>
+		<div className='dark_mode'>
+			<input
+				className='dark_mode_input'
+				type='checkbox'
+				id='darkmode-toggle'
+				onChange={toggleTheme}
+				defaultChecked={selectedTheme === "dark"}
+			/>
+			<label
+				className='dark_mode_label'
+				htmlFor='darkmode-toggle'></label>
 		</div>
 	);
 };
 
-export default ColorModeToggle;
+export default DarkMode;
