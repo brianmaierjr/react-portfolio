e208b11b24d11b17c68e1bc6741e504de8204657
Brian Maier <brimaidesigns@gmail.com>
2024-10-11 07:52:36 -0400

    make section component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,40 @@
 import AppFooter from "../components/app-footer";
 import AppHeader from "../components/app-header";
-import Career from "../components/career";
-import CurrentFocus from "../components/current-focus";
 import Project from "../components/projects/project";
 import Projects from "../components/projects/projects";
-
+import Section from "../components/section";
+import styles from "./home.module.css";
 const Home = () => {
 	return (
 		<div className='container'>
 			<AppHeader />
 			<main>
-				<Career />
-				<CurrentFocus />
-				<Projects>
+				<Section label='Work'>
+					<h2>Work</h2>
+					<p>
+						I currently work for{" "}
+						<a href='https://cloverlearning.com' target='_blank'>
+							Clover Learning
+						</a>{" "}
+						as a Design Engineer. I work daily with React and Vue to
+						create components and a design system that lets me and
+						the other engineers build our app, marketing sites, and
+						future mobile app.
+					</p>
+				</Section>
+				<Section label='Now'>
+					<h2>Now</h2>
+					<ul className={styles.list}>
+						<li>Transitioning from Vue(Nuxt) to React</li>
+						<li>Building out the Clover Learning Design System</li>
+						<li>Converting Long Haul Jekyll Theme to Astro</li>
+						<li>Lifting Weights 6+ days a week</li>
+						<li>Practicing Guitar 1 hour each day</li>
+						<li>Reading the Bible every day</li>
+					</ul>
+				</Section>
+				<Section label='Projects'>
+					<h2>Personal Projects</h2>
 					<Project
 						name='147 Colors'
 						description='Astro site created to showcase all of the CSS color names that are available.'
@@ -28,7 +50,7 @@ const Home = () => {
 						description='Minimal, type-focused, blogging theme built for Jekyll. Astro version coming soon.'
 						href='https://brianmaierjr.com/long-haul'
 					/>
-				</Projects>
+				</Section>
 			</main>
 			<AppFooter />
 		</div>
